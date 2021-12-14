77af985b3631d00c5ec30e3403b53bbee5770279
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-12-14 19:41:14 +0300

    added TripModel

---
+++ TripNotes/Extensions/Extension + UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  Extension + UIColor.swift
+//  TripNotes
+//
+//  Created by Владислав Сизонов on 13.12.2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tripRed = UIColor(red: 253/255, green: 141/255, blue: 122/255, alpha: 1)
+    static let tripBlue = UIColor(red: 48/255, green: 57/255, blue: 82/255, alpha: 1)
+    static let tripYellow = UIColor(red: 247/255, green: 215/255, blue: 148/255, alpha: 1)
+    static let tripGrey = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1)
+    static let tripWhite = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+}
---
+++ TripNotes/TabBarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarViewController.swift
+//  TripNotes
+//
+//  Created by Владислав Сизонов on 13.12.2021.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let firstViewController = UINavigationController(rootViewController: TripsViewController()) 
+        let secondViewController = UINavigationController(rootViewController: FavouritesViewController())
+        
+        firstViewController.tabBarItem.title = "Trips"
+        firstViewController.tabBarItem.image = UIImage(systemName: "pencil")
+        secondViewController.tabBarItem.title = "Favourites"
+        secondViewController.tabBarItem.image = UIImage(systemName: "circle.fill")
+        viewControllers = [firstViewController, secondViewController]
+    }
+    
+}
