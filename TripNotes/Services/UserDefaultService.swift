5b7b69c20dd7bced4b371ebc96925cb654bfd1d5
VLADISLAV <SIZONOV.95@MAIL.RU>
2022-06-02 23:23:48 +0300

    finished userDefaults service

--- TripNotes/Services/UserDefaultService.swift
+++ TripNotes/Services/UserDefaultService.swift
@@ -1,5 +1,5 @@
 //
-//  UserDefaultService.swift
+//  UserDefaultsService.swift
 //  TripNotes
 //
 //  Created by Владислав Сизонов on 30.05.2022.
@@ -7,10 +7,31 @@
 
 import Foundation
 
-protocol UserDefaultServiceProtocol {
-    
+protocol UserDefaultsServiceProtocol {
+    func save(_ countryOrCity: String)
+    func getCityOrCountry() -> String
+    func clearData()
 }
 
-final class UserDefaultService: UserDefaultServiceProtocol {
+final class UserDefaultsService: UserDefaultsServiceProtocol {
     
+    // MARK: Private properties
+    
+    private lazy var userDefaults = UserDefaults.standard
+    private lazy var countryKey = C.UserDefaultsKeys.countryKey.rawValue
+    
+    
+    // MARK: Methods
+    
+    func save(_ countryOrCity: String){
+        userDefaults.set(countryOrCity, forKey: countryKey)
+    }
+    
+    func getCityOrCountry() -> String {
+        return userDefaults.object(forKey: countryKey) as? String ?? ""
+    }
+    
+    func clearData(){
+        userDefaults.removeObject(forKey: countryKey)
+    }
 }
